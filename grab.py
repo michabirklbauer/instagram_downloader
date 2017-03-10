f46653c7d28a42de45dbe1f8972da65a8de5431b
Micha Birklbauer <micha.birklbauer@gmail.com>
2017-03-10 03:35:49 +0100

    added python script for multiple pictures

---
+++ grab.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+if len(sys.argv) == 2:
+	FILE = sys.argv[1]
+	try:
+		links = open(FILE, "r").readlines()
+	except FileNotFoundError:
+		print(FILE, " doesn't exist!")
+		quit()
+	for link in links:
+		cmd = "./instagram_download.sh "+str(link)
+		os.system(cmd)
